2b98ce31f3c10a21ebf05c8afe420af549fa51c6
VangaAkhil <143966708+VangaAkhil@users.noreply.github.com>
2023-09-25 21:09:00 -0500

    Update task5.py
    added test cases

--- task5.py
+++ task5.py
@@ -33,3 +33,17 @@ studentName = student_database["student1"]["name"]
 studentAge = student_database["student1"]["age"]
 
 print(f"Student info:\nName: {studentName}\nAge: {studentAge}")
+
+
+#Test Cases
+def test1():
+  assert isinstance(favorite_books, list)
+  assert favorite_books[0]['title'] == "half girlfriend"
+  assert favorite_books[1]['author'] == "chintu"
+  assert favorite_books[2]['title'] == "baby"
+
+
+def test2():
+  assert isinstance(student_database, dict)
+  assert student_database['student1']['name'] == "munna"
+  assert student_database['student3']['age'] == 26
